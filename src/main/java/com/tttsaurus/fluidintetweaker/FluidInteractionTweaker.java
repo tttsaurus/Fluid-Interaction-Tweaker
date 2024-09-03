a1b9fd89501a6f425b5b3580ba0c93661e314f27
tttsaurus <chenh1231y@gmail.com>
2024-09-03 13:32:29 +0800

    v1.2.2 update

--- src/main/java/com/tttsaurus/fluidintetweaker/FluidInteractionTweaker.java
+++ src/main/java/com/tttsaurus/fluidintetweaker/FluidInteractionTweaker.java
@@ -13,7 +13,7 @@ public class FluidInteractionTweaker
 {
     public static final String MODID = "fluidintetweaker";
     public static final String NAME = "Fluid Interaction Tweaker";
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.2";
 
     @SidedProxy(
             clientSide = "com.tttsaurus.fluidintetweaker.proxy.ClientProxy",
