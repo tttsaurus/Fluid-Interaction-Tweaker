9075ff65094dc856ed8571ccce1d0efcbfd85ea2
tttsaurus <chenh1231y@gmail.com>
2024-09-28 16:53:38 -0400

    v1.4.0-preview-1 update

--- src/main/java/com/tttsaurus/fluidintetweaker/FluidInteractionTweaker.java
+++ src/main/java/com/tttsaurus/fluidintetweaker/FluidInteractionTweaker.java
@@ -17,7 +17,7 @@ public final class FluidInteractionTweaker
 {
     public static final String MODID = "fluidintetweaker";
     public static final String NAME = "Fluid Interaction Tweaker";
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "1.4.0-preview-1";
 
     public static final boolean IS_JEI_LOADED = Loader.isModLoaded("jei");
     public static final boolean IS_MODULARUI_LOADED = Loader.isModLoaded("modularui");
